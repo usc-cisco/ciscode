f7908ff7a2475bd51c5d756154aa8523d747617b
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-07-25 11:03:27 +0800

    feat: added problem site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,41 +8,9 @@ import { checkCode } from "@/lib/fetchers/code.fetchers";
 import { useState } from "react";
 
 export default function Home() {
-  const [code, setCode] = useState<string>("");
-
-  const handleCodeChange = (value: string | undefined) => {
-    setCode(value || "");
-  };
-
-  const onSubmit = async () => {
-    const input = ""; // You can replace this with actual input if needed
-    const { output, error } = await checkCode(code, input);
-    if (error) {
-      console.error("Error checking code:", error);
-    } else {
-      console.log("Output:", output);
-    }
-  };
-
-  const problem = {
-    title: "Sample Problem",
-    author: "John Doe",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-  }
-
   return (
     <div>
-      <div>
-        <SplitView 
-          sizes={[25, 50, 25]}
-        >
-          <ProblemBar problem={problem}/>
-          <CodeEditor 
-            handleCodeChange={handleCodeChange}
-          />
-          <TestCaseBar onSubmit={onSubmit} />
-        </SplitView>
-      </div>
+      <p>This is the home page</p>
     </div>
-  );
+  )
 }
