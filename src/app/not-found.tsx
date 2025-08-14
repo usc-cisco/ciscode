adfec301e66a8483053309a021b25c91eaf9ddb6
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-08-15 02:29:47 +0800

    refactor: changed logout to dropdown

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,16 @@
+"use client";
+
+import { useAuth } from '@/contexts/auth.context';
 import Link from 'next/link'
 import React from 'react'
 
 const NotFound = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <div className='w-full min-h-[calc(100vh-4rem)] flex flex-col items-center justify-center gap-4'>
       <p className='font-semibold text-lg'>This page is under construction...</p>
-      <Link className='rounded-lg bg-primary text-white px-6 py-2' href="/">Return Home</Link>
+      <Link className='rounded-lg bg-primary text-white px-6 py-2' href={isAuthenticated ? "/home" : "/"}>Return Home</Link>
     </div>
   )
 }
--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -4,11 +4,12 @@ import React from 'react'
 import { ThemeToggle } from './theme-toggle'
 import Brand from './brand'
 import { useAuth } from '@/contexts/auth.context';
-import { LogOutIcon } from 'lucide-react';
+import { LogOutIcon, User } from 'lucide-react';
 import { Button } from '../ui/button';
 import { Badge } from '../ui/badge';
 import RoleEnum from '@/lib/types/enums/role.enum';
 import { useRouter } from 'next/navigation';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem } from '../ui/dropdown-menu';
 
 const Navbar = () => {
   const { isAuthenticated, clearAuth, userInfo, isAdmin } = useAuth();
@@ -24,10 +25,10 @@ const Navbar = () => {
   return (
     <div className='w-full h-16 px-6 flex justify-between items-center bg-primary text-primary-foreground'>
       <Brand path={isAuthenticated ? "/home" : "/"}/>
-      <div className='flex items-center gap-4'>
+      <div className='flex items-center gap-2'>
         {
           isAuthenticated && role !== null && (
-            <Badge onClick={isAdmin ? () => router.push('/admin') : () => {}} variant={'default'} className={`text-xs bg-primary-foreground text-primary rounded-xl ${isAdmin ? 'cursor-pointer' : 'pointer-events-none'}`}>
+            <Badge onClick={isAdmin ? () => router.push('/admin') : () => {}} variant={'default'} className={`text-xs bg-primary-foreground text-primary rounded-xl mx-1 ${isAdmin ? 'cursor-pointer' : 'pointer-events-none'}`}>
               {
                 role === RoleEnum.USER ?
                 "STUDENT"
@@ -39,9 +40,25 @@ const Navbar = () => {
         }
         <ThemeToggle />
         {isAuthenticated && (
-          <Button onClick={handleLogout} size="icon" className="bg-primary hover:bg-primary hover:brightness-80 ring-0 hover:ring-0 shadow-none text-primary-foreground hover:text-primary-foreground cursor-pointer">
-            <LogOutIcon className='size-4'/>
-          </Button>
+          // <Button onClick={handleLogout} size="icon" className="bg-primary hover:bg-primary hover:brightness-80 ring-0 hover:ring-0 shadow-none text-primary-foreground hover:text-primary-foreground cursor-pointer">
+          //   <LogOutIcon className='size-4'/>
+          // </Button>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size="icon" className="bg-primary hover:bg-primary hover:brightness-80 ring-0 hover:ring-0 shadow-none text-primary-foreground hover:text-primary-foreground cursor-pointer">
+                <User />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='bg-vscode-light dark:bg-vscode-dark text-sm' align="end">
+              <DropdownMenuItem onClick={() => router.push('/profile')}>
+                Profile
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={handleLogout}>
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         )}
       </div>
     </div>
