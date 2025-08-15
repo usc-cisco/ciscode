9c6d3b5d13e7893d9d39c7144c200cc464a1146f
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-08-15 14:05:55 +0800

    feat: added reset to default

--- src/components/problem/code-editor.tsx
+++ src/components/problem/code-editor.tsx
@@ -2,18 +2,21 @@ import { Editor } from '@monaco-editor/react';
 import { useTheme } from 'next-themes';
 import React from 'react'
 import ProblemCard from './problem-card';
-import { CodeXml } from 'lucide-react';
+import { CodeXml, RefreshCcw } from 'lucide-react';
+import { HoverCard, HoverCardContent, HoverCardTrigger } from '../ui/hover-card';
+import { Button } from '../ui/button';
 
 interface CodeEditorProps {
-    defaultCode?: string;
+    code: string;
     onCodeChange: (value: string | undefined) => void;
+    handleReset: () => void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({ onCodeChange, defaultCode }) => {
+const CodeEditor: React.FC<CodeEditorProps> = ({ onCodeChange, code, handleReset }) => {
     const { resolvedTheme } = useTheme();
 
   return (
-    <ProblemCard className='overflow-hidden'>
+    <ProblemCard className='overflow-hidden relative'>
       <div className='px-2 py-2 rounded-t-xl border-b border-gray-200 dark:border-gray-700 flex justify-between items-center'>
         <div className='py-1 px-2 bg-vscode-light dark:bg-vscode-dark flex items-center gap-1 rounded-lg hover:brightness-90 dark:hover:brightness-110 cursor-pointer'>
           <CodeXml className='size-4 text-primary' />
@@ -23,7 +26,7 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onCodeChange, defaultCode }) =>
       <Editor
           theme={resolvedTheme === 'dark' ? 'vs-dark' : 'light'}
           height="100%"
-          defaultValue={defaultCode || ''}
+          value={code}
           defaultLanguage="c"
           onChange={onCodeChange}
           options={{
@@ -33,6 +36,16 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onCodeChange, defaultCode }) =>
               lineNumbersMinChars: 2,
           }}
       />
+      <HoverCard>
+        <HoverCardTrigger className='absolute bottom-4 right-6'>
+          <Button onClick={handleReset} className='rounded-full size-12 flex justify-center items-center opacity-50 hover:opacity-100 transition-opacity cursor-pointer'>
+            <RefreshCcw className='size-6' />
+          </Button>
+        </HoverCardTrigger>
+        <HoverCardContent>
+          <p className='text-xs'>Reset the code to the default version.</p>
+        </HoverCardContent>
+      </HoverCard>
     </ProblemCard>
   );
 }
