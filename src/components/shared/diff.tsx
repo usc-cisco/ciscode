d78acff11d6bb4dcd067c52a645fa94bece6e2da
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-08-15 22:23:16 +0800

    fix: fixed see whats wrong text color

--- src/components/shared/diff.tsx
+++ src/components/shared/diff.tsx
@@ -5,7 +5,7 @@ export function getHighlightedDiff(oldStr: string, newStr: string) {
     let color = "";
     if (part.added)
       color =
-        "bg-green-200 text-green-800 dark:bg-green-800 dark:text-bg-green-200"; // Added text
+        "bg-green-200 text-green-800 dark:bg-green-800 dark:text-green-200"; // Added text
     if (part.removed)
       color = "bg-red-200 text-red-800 dark:bg-red-800 dark:text-red-200"; // Removed text
 
