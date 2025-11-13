77451799fb0883fb39157c5c8163e8204b8b7ce9
Matt Cabarrubias <monapwc2020@gmail.com>
2025-11-13 23:28:43 +0800

    feat: updated the Prog 2 color to yellow to match the prog 2 stats bar color

--- src/lib/types/enums/difficulty.enum.ts
+++ src/lib/types/enums/difficulty.enum.ts
@@ -9,7 +9,7 @@ export const getDifficultyColor = (difficulty: DifficultyEnum): string => {
     case DifficultyEnum.PROG1:
       return "bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200";
     case DifficultyEnum.PROG2:
-      return "bg-blue-100 text-blue-800 dark:bg-blue-900 dark:text-blue-200";
+      return "bg-yellow-100 text-yellow-800 dark:bg-yellow-900 dark:text-yellow-200";
     case DifficultyEnum.DSA:
       return "bg-red-100 text-red-800 dark:bg-red-900 dark:text-red-200";
     default:
