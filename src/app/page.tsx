0e803b382104c6979c6e45818f386dc360a8ce6c
Zeus Elderfield <elderfieldzeus24@gmail.com>
2025-11-26 18:40:08 +0800

    style: added ciscode logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import LoginForm from "@/components/auth/login-form";
 import { useAuth } from "@/contexts/auth.context";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { toast } from "sonner";
@@ -25,9 +26,15 @@ export default function Auth() {
     <div className="h-cscreen w-full flex flex-col items-center">
       <div className="flex flex-col h-cscreen w-full max-w-4xl justify-center items-center gap-8 px-6 md:flex-row md:items-center md:justify-between">
         <div className="-mt-24  md:max-w-2/5">
-          <h1 className="text-primary text-center md:text-start font-semibold text-6xl">
-            ciscode
-          </h1>
+          <div className="flex w-full justify-center md:justify-start">
+            <Image
+              src="/ciscode-logo.png"
+              alt="Ciscode Logo"
+              width={500}
+              height={500}
+              className="h-12 w-auto md:-ml-1"
+            />
+          </div>
           <p className="font-normal text-center md:text-start text-xl">
             Code your problems away...
           </p>
